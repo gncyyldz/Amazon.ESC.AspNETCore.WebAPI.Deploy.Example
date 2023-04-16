a7e51f471bce73ac19dc18758c5e05ada8d5af2b
Gençay Yıldız <gyildizmail@gmail.com>
2023-04-16 13:46:58 +0300

    Add project files.

---
+++ Amazon.ESC.AspNETCore.WebAPI.Deploy.Example.API/Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.ESC.AspNETCore.WebAPI.Deploy.Example.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace Amazon.ESC.AspNETCore.WebAPI.Deploy.Example.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        readonly IMongoCollection<Book> _bookCollection;
+        public BooksController(IConfiguration configuration)
+        {
+            MongoClient mongoClient = new(configuration["MongoDBConfigurations:ConnectionString"]);
+            IMongoDatabase database = mongoClient.GetDatabase(configuration["MongoDBConfigurations:Database"]);
+            _bookCollection = database.GetCollection<Book>(configuration["MongoDBConfigurations:BookCollection"]);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+            => Ok(await _bookCollection.Find(b => true)
+                                    .ToListAsync());
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Book book)
+        {
+            await _bookCollection.InsertOneAsync(book);
+            return Ok(book.Id);
+        }
+    }
+}
---
+++ Amazon.ESC.AspNETCore.WebAPI.Deploy.Example.API/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Amazon.ESC.AspNETCore.WebAPI.Deploy.Example.API.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public string? PublishedYear { get; set; }
+    }
+}
